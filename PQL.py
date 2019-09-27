c44b41d5a6e539f79680914f3bc629f958624555
reagul <reagul.2007@gmail.com>
2019-09-27 17:57:38 -0400

    data key

--- PQL.py
+++ PQL.py
@@ -27,7 +27,7 @@ def mainloop():
         print(results.keys())
         ##[u'status', u'data']
         print("^^^^^^^^^")
-        cpuutil = json.loads(results.key('data'))
+        cpuutil = json.loads(results['data'])
         print(cpuutil.keys())
         print("@@@@@@@@@")
         currentDT = datetime.datetime.now()

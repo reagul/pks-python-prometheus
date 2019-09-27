254c2e266cedc65a9cf3ba7011efdd56961508f9
reagul <reagul.2007@gmail.com>
2019-09-27 17:08:04 -0400

    date time added

--- PQL.py
+++ PQL.py
@@ -7,17 +7,13 @@ PROMETHEUS = 'http://prometheus.my-clusterapps.corp.local/'
 
 def mainloop():
 
-    currentDT = datetime.datetime.now()
-    end_of_month = datetime.datetime.today().replace(day=1).date()
-    # Last day of the previous month.
-    last_day = end_of_month - datetime.timedelta(days=1)
-    duration = '[' + str(last_day.day) + 'd]'
     while True:
     #'query': 'sum by (job)(increase(process_cpu_seconds_total' + duration + '))',
         response = requests.get(PROMETHEUS + '/api/v1/query',
         params={ 'query': '1 - avg(rate(node_cpu_seconds_total{mode="idle"}[1m]))'})
         results = response.json()
         print (results)
+        currentDT = datetime.datetime.now()
         print ("Current Second is: %d" % currentDT.second)
         time.sleep(3)
 if __name__== "__main__":

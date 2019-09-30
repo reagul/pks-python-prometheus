a859f7db065a69c16978eddd6058b1d2965f1c94
reagul <reagul.2007@gmail.com>
2019-09-30 10:50:16 -0400

    priont exitcodes

--- pks-client.py
+++ pks-client.py
@@ -7,5 +7,12 @@ popen.wait()
 output, err = popen.communicate()
 exitcode = popen.returncode
 print ("err" + str(err))
-print ("out" + str(output))
+print ("out put" + str(output))
 print ("exitcode" + str(exitcode))
+
+if err == 'None':
+    print("No err")
+else if exitcode != 0:
+    print("exit code was NON zero")
+else  exitco == 0:
+    print("exit code was zero")

6460b5f54aa5704bd37470a07ab60f53b23e314b
reagul <reagul.2007@gmail.com>
2019-09-30 14:47:28 -0400

    PQL V2

--- PQL.py
+++ PQL.py
@@ -47,6 +47,7 @@ def mainloop():
                 cpupercent = (float(cpuutil))
                 cpufinal = int(round(cpupercent,2) * 100)
                 print("CPU-UTIL % ==" + str(cpufinal))
+                numNodes()
                 if cpufinal > 75 :
                     ##print("CPU util over 75 so sleep for 1mins")
                     ##time.sleep(60)

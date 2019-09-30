e1c583870d64f3fda18438b132e8eae9cf852ffd
reagul <reagul.2007@gmail.com>
2019-09-30 10:24:55 -0400

    popen exitcode

--- pks-client.py
+++ pks-client.py
@@ -4,8 +4,7 @@ import subprocess
 args = ("pks","login","-a","pks.corp.local","-u","pksadmin","-k","-p","VMware1!")
 popen = subprocess.Popen(args, stdout=subprocess.PIPE)
 popen.wait()
-out, err = popen.communicate()
-exitcode = popen.returncode
-print(exitcode)
-output = popen.stdout.read()
-print output
+output, err = popen.communicate()
+##exitcode = popen.returncode
+print err
+print out
